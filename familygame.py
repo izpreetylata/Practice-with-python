8f06e2c76db77fb2ae88efc2498c0c2b74f1e80d
izpreetylata <70439433+izpreetylata@users.noreply.github.com>
2020-10-29 02:15:41 +0600

    Add files via upload

---
+++ familygame.py
@@ -0,0 +1,20 @@
+import turtle
+t= turtle.Pen()
+turtle.bgcolor('black')
+colors=['red','yellow','blue','green','orange','purple''white','brown','gray','pink']
+family=[]
+name=turtle.textinput('My family','enter a name or just hit [ENTER] to end:')
+while name !='':
+    family.append(name)
+    
+    name=turtle.textinput('My family',
+                          'enter a name or just hit [ENTER] to end:')
+
+
+for x in range(100):
+    t.pencolor(colors[x%len(family)])
+    t.penup()
+    t.forward(x*4)
+    t.pendown()
+    t.write(family[x%len(family)],font=('Arial',int((x+4)/4),'bold'))
+    t.left(360/len(family)+2)
